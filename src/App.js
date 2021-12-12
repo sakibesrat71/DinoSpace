5987f3f1e3f2cd7213613c4fad29c5b90829cd76
acesuu <ahnafakib@iut-dhaka.edu>
2021-12-12 19:57:18 +0600

    added search bar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './index.css';
 import dine from './dine2.jpg';
 import Navbar from './navbar'
 import Restaurant from './restaurants';
+import {Form, FormControl, Button} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
         <img src={dine} />
-        <p className='moto'>NEVER WAIT TO EAT AGAIN</p>   
+        <p className='moto'>NEVER WAIT TO EAT AGAIN</p>
+        <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>   
       <Restaurant/>
     </div>
         
