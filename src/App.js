805794437b07fde668c798fa3bdd3d181b0a9c8e
acesuu <ahnafakib@iut-dhaka.edu>
2021-12-12 18:38:41 +0600

    added restaurant list and horizontal scrolling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './index.css';
 import dine from './dine2.jpg';
+import Navbar from './navbar'
+import Restaurant from './restaurants';
 
-import Navbar from'./navbar'
 function App() {
   return (
-    <div className="App">  
+    <div className="App">
       <Navbar />
-      <div className="moto">
-        <img src={dine}/>
-        <p>NEVER WAIT TO EAT AGAIN</p>
-      </div>
+        <img src={dine} />
+        <p className='moto'>NEVER WAIT TO EAT AGAIN</p>   
+      <Restaurant/>
     </div>
+        
   );
 }
 
---
+++ src/restaurants.js
@@ -0,0 +1,117 @@
+import './index.css';
+import { Card, CardGroup } from 'react-bootstrap';
+import dine from './dine.jpg'
+import dine3 from './dine3.jpg'
+import dine4 from './dine4.jpg'
+import Carousel from 'react-bootstrap/Carousel';
+
+const restaurant = () => {
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <CardGroup>
+                    <Card>
+                        <Card.Img variant="top" src={dine} />
+                        <Card.Body>
+                            <Card.Title>Chillox</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={dine3} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={dine4} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </Carousel.Item>
+            <Carousel.Item>
+                <CardGroup>
+                    <Card>
+                        <Card.Img variant="top" src={dine} />
+                        <Card.Body>
+                            <Card.Title>Amari</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={dine3} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={dine4} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </Carousel.Item>
+            <Carousel.Item>
+                <CardGroup>
+                    <Card>
+                        <Card.Img variant="top" src={dine} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={dine3} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src={dine4} />
+                        <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
+
+export default restaurant;
