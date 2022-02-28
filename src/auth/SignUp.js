1c35cf0dde4d1be9c7918deacb78e7df3f1212d9
acesuu <ahnafakib@iut-dhaka.edu>
2022-02-28 11:06:25 +0600

    added facebook option to sign in and sign up

--- src/auth/SignUp.js
+++ src/auth/SignUp.js
@@ -1,5 +1,6 @@
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import img from "./login4.jpg";
+// import {}
 import "../css/auth.css";
 import userIcon from "./user.png";
 import Aos from "aos";
@@ -12,18 +13,18 @@ const SignUp = () => {
         Aos.init({ duration: 1500 });
     }, []);
 
-    const[user, setUser] = useState({
-        name:"",email:"",phone:"",pass:"",rpass:""
+    const [user, setUser] = useState({
+        name: "", email: "", phone: "", pass: "", rpass: ""
     });
 
     let name, value;
 
-    const handleInputs = (e) =>{
+    const handleInputs = (e) => {
         console.log(e);
-        name= e.target.name;
-        value= e.target.value;
+        name = e.target.name;
+        value = e.target.value;
 
-        setUser({...user, [name]:value});
+        setUser({ ...user, [name]: value });
     }
     return (
         <Container>
@@ -43,10 +44,10 @@ const SignUp = () => {
                                     <img className="user" src={userIcon} />
                                 </div>
                                 <Form.Group className="mb-3">
-                                    <Form.Control type="text" name="name" placeholder="Enter Name" id="name" value={user.name} onChange={handleInputs}/>
+                                    <Form.Control type="text" name="name" placeholder="Enter Name" id="name" value={user.name} onChange={handleInputs} />
                                 </Form.Group>
                                 <Form.Group className="mb-3">
-                                    <Form.Control type="text" placeholder="Enter Mobile Number" name="phone" id="phone" value={user.phone} onChange={handleInputs}/>
+                                    <Form.Control type="text" placeholder="Enter Mobile Number" name="phone" id="phone" value={user.phone} onChange={handleInputs} />
                                 </Form.Group>
                                 <Form.Group className="mb-3" controlId="formBasicEmail">
                                     <Form.Control type="email" placeholder="Enter email" name="email" id="email" value={user.email} onChange={handleInputs} />
@@ -60,6 +61,13 @@ const SignUp = () => {
                                 <Button variant="primary" type="submit">
                                     Submit
                                 </Button>
+                                <br/>
+                                <p className="emnei">Or sign up using</p>
+                                {/* <i class="bi bi-facebook"></i> */}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
+                                </svg>
+
                             </div>
                         </Form>
                     </div>
