8833805f410073dd934129de00d6badf2bf36810
acesuu <ahnafakib@iut-dhaka.edu>
2022-03-01 16:50:47 +0600

    merged search with backend

--- src/FrontPage/search.js
+++ src/FrontPage/search.js
@@ -1,20 +1,57 @@
+import React from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../css/index.css';
 import {Form, FormControl, Button} from 'react-bootstrap';
-const searchbar = () => {
+
+
+const Searchbar = () => {
+
+  const [restaurentDetails,setRestaurentDetails] = useState([])
+
+   //  const {state,dispatch} = useContext(UserContext)
+
+
+     const fetchUsers = async (query)=>{
+       const queryObj = {
+           query:query
+          };
+       try{
+         console.log(query);
+        const response = await axios.post('post/search', queryObj);
+        setRestaurentDetails(response.data);
+        console.log(response);
+      }catch(error){
+        console.log(error);
+      }
+
+     }
     return (
       <div className="form">
         <Form className="d-flex">
         <FormControl
-          type="search"
+          type="text"
           placeholder="Search"
           className="me-2"
           aria-label="Search"
+          onChange={(e)=>fetchUsers(e.target.value)}
         />
-        <Button variant="outline-success">Search</Button>
+        {/* <Button variant="outline-success">Search</Button> */}
+        <ul>
+          {
+            restaurentDetails.map(restaurent=>(
+              <li key={restaurent._id}>
+              {restaurent.name}
+              <Link to={`/restaurant/${restaurent._id}`}>View Details</Link>
+              </li>
+            ))
+          }
+          </ul>
       </Form> 
       </div>
         
       );
 }
  
-export default searchbar;
+export default Searchbar;
