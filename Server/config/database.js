e189e8dc29b23db558a2486536177704bcc6d849
Salman Ahsan <salmanahsan@iut-dhaka.edu>
2022-03-01 12:05:57 +0600

    added bcrypt functionalities in database config insetad of server route

--- Server/config/database.js
+++ Server/config/database.js
@@ -30,6 +30,19 @@ const userSchema = mongoose.Schema({
 
 userSchema.plugin(findOrCreate);
 //create a model for the schema
-const userModel = mongoose.model('User', userSchema);    //database store users
-
-module.exports = userModel;
+userSchema.methods.matchPassword = async function (enteredPassword) {
+ return await bcrypt.compare(enteredPassword, this.password);
+};
+  
+  // will encrypt password everytime its saved
+ userSchema.pre("save", async function (next) {
+ if (!this.isModified("password")) {
+     next();
+}
+ const salt = await bcrypt.genSalt(10);
+ this.password = await bcrypt.hash(this.password, salt);
+ });
+  
+ const User = mongoose.model("User", userSchema);
+  
+export default User;
